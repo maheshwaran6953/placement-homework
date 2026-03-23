8bf71635f4688536b1737899c33d57373deff165
MAHESHWARAN P <maheshwaranpalanisamy1@gmail.com>
2026-03-23 14:39:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.java
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.java
@@ -1,19 +1,26 @@
 class Solution {
     public int[] leftRightDifference(int[] nums) {
         int n = nums.length, total = 0;
-        int[] leftSum = new int[n], rightSum = new int[n];
-        for(int i = 0; i < n; i++) {
-            total += nums[i];
+        // int[] leftSum = new int[n], rightSum = new int[n];
+        for(int i : nums) {
+            total += i;
         }
-        int subTotal = total;
+        // int subTotal = total;
+        // for(int i = 0; i < n; i++){
+        //     rightSum[i] = total - nums[i];
+        //     leftSum[n - 1 - i] = subTotal - nums[n - i - 1];
+        //     total -= nums[i];
+        //     subTotal -= nums[n - 1 - i];
+        // }
+        // for(int i = 0; i < nums.length; i++) {
+        //     nums[i] = Math.abs(leftSum[i] - rightSum[i]);
+        // }
+        int leftSum = 0;
         for(int i = 0; i < n; i++){
-            rightSum[i] = total - nums[i];
-            leftSum[n - 1 - i] = subTotal - nums[n - i - 1];
-            total -= nums[i];
-            subTotal -= nums[n - 1 - i];
-        }
-        for(int i = 0; i < nums.length; i++) {
-            nums[i] = Math.abs(leftSum[i] - rightSum[i]);
+            int original = nums[i];
+            int rightSum = total - leftSum - original;
+            nums[i] = Math.abs(leftSum - rightSum);
+            leftSum += original;
         }
         return nums;
     }
