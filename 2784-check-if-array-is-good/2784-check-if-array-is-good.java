c025d46dbdff11888c96a2cadbf6ae8141b595f4
MAHESHWARAN P <maheshwaranpalanisamy1@gmail.com>
2026-06-08 17:07:04 +0530

    Time: 7 ms (12.98%), Space: 45.6 MB (8.33%) - LeetHub

--- 2784-check-if-array-is-good/2784-check-if-array-is-good.java
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.java
@@ -1,16 +1,15 @@
 class Solution {
     public boolean isGood(int[] nums) {
-        int n = nums.length - 1, max = 0;
-        for(int i : nums)
-            if(i > max)
-                max = i;
-        if(max != n) return false;
-        int count[]  = new int[n + 1];
-        for(int i : nums)
-            count[i]++;
-        for (int i = 1; i < n; i++)
-            if(count[i] != 1)
+        Map<Integer, Integer> m = new HashMap<>();
+        Arrays.sort(nums);
+        int max = nums[nums.length - 1];
+        for(int i : nums){
+            m.put(i, m.getOrDefault(i, 0) + 1);
+        }
+        for(int i = 1; i < nums.length - 1; i++){
+            if(m.getOrDefault(i, 0) != 1)
                 return false;
-        return count[n] == 2;
+        }
+        return m.getOrDefault(nums.length - 1, 0) == 2;
     }
 }
